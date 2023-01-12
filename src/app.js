808eb17ff7b8594473de6bfdcf38377fafee8928
encarnacao <cencarnacaoq@gmail.com>
2023-01-12 18:38:37 -0300

    feat: add get method to edo through query strings

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 import { exec } from "child_process";
 import { promisify } from "util";
-import path from 'path'
+import path from "path";
 import express from "express";
 import Joi from "joi";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const odeSchema = Joi.object({
-    equation: Joi.string().required(),
-    x0: Joi.number().required(),
-    y0: Joi.number().required(),
-    dy0: Joi.number().required(),
-    step: Joi.number().required(),
-    max: Joi.number().required(),
+	equation: Joi.string().required(),
+	x0: Joi.number().required(),
+	y0: Joi.number().required(),
+	dy0: Joi.number().required(),
+	step: Joi.number().required(),
+	max: Joi.number().required(),
 });
 
 const app = express();
@@ -22,43 +22,71 @@ app.use(express.json());
 const PORT = 5000;
 
 const execPromise = promisify(exec);
-async function pythonScript(equation,x0,y0,dy0,step,n){
-    console.log("executando script");
-    const { stdout, stderr } = await execPromise(`python ./src/script.py ${equation} ${x0} ${y0} ${dy0} ${step} ${n}`);
+async function pythonScript(equation, x0, y0, dy0, step, n) {
+	console.log("executando script");
+	const { stdout, stderr } = await execPromise(
+		`python ./src/script.py ${equation} ${x0} ${y0} ${dy0} ${step} ${n}`
+	);
 }
-async function installRequirements(){
-    console.log("instalando requirements");
-    const { stdout, stderr } = await execPromise(`pip install -r ./requirements.txt`);
-    return stdout;
+async function installRequirements() {
+	console.log("instalando requirements");
+	const { stdout, stderr } = await execPromise(
+		`pip install -r ./requirements.txt`
+	);
+	return stdout;
 }
-app.post("/ode", (req,res) =>{
-    const { error } = odeSchema.validate(req.body);
-    if(error){
-        res.status(400).send(error.details[0].message);
-        return;
-    }
-    const { equation, x0, y0, dy0, step, max } = req.body;
-    const n = parseInt(Number(max)/Number(step));
-    pythonScript(equation,x0,y0,dy0,step,n).then(()=>{
-        res.status(201).sendFile(path.join(__dirname, '../images', 'plot.png'),(err)=>{
-            if(err){
-                console.log(err);
-            } else{
-                console.log("Imagem enviada");
-            }
-        })
-    }
-    );
-})
+app.post("/ode", (req, res) => {
+	const { error } = odeSchema.validate(req.body);
+	if (error) {
+		res.status(400).send(error.details[0].message);
+		return;
+	}
+	const { equation, x0, y0, dy0, step, max } = req.body;
+	const n = parseInt(Number(max) / Number(step));
+	pythonScript(equation, x0, y0, dy0, step, n).then(() => {
+		res.status(201).sendFile(
+			path.join(__dirname, "../images", "plot.png"),
+			(err) => {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log("Imagem enviada");
+				}
+			}
+		);
+	});
+});
+
+app.get("/edo", async (req, res) => {
+	const { equation, x0, y0, dy0, step, max } = req.query;
+	const n = parseInt(Number(max) / Number(step));
+	try {
+		await pythonScript(equation, x0, y0, dy0, step, n);
+	} catch {
+		res.sendStatus(500);
+		return;
+	} finally {
+		res.status(201).sendFile(
+			path.join(__dirname, "../images", "plot.png"),
+			(err) => {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log("Imagem enviada");
+				}
+			}
+		);
+	}
+});
 
 //Testing route
-app.get("/", (_,res)=>{
-    res.send("Hello World");
+app.get("/", (_, res) => {
+	res.send("Hello World");
 });
 
 installRequirements().then(() => {
-    console.log("requirements instalados")
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+	console.log("requirements instalados");
+	app.listen(PORT, () => {
+		console.log(`Server running on port ${PORT}`);
+	});
 });
